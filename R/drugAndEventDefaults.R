9c7e44f3b894b4b0d14313fe68f9438110f4a167
Dean Attali <dean@attalitech.com>
2026-01-26 16:06:51 -0500

    Add blank lines for improved readability

--- R/drugAndEventDefaults.R
+++ R/drugAndEventDefaults.R
@@ -18,6 +18,8 @@ getDrugDefaultsGlobal <- function(expand = TRUE)
     system.file("extdata", "drugDefaults_global.csv", package = "stanpumpR"),
     na.strings = ""
   )
+
+  
   if (expand) {
     drugDefaultsDataset$Units <- drugUnitsExpand(drugDefaultsDataset$Units)
   }
