8855efa2dde1999f979cbad9c489934db59b2a90
Dean Attali <daattali@gmail.com>
2026-02-07 16:03:28 -0500

    Update help link text in app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,7 +14,7 @@ app_ui <- function() {
           tags$a(
             href = config$help_link,
             id = "help_link",
-            "Examples and Help AAA",
+            "Examples and Help AAABB",
             target = "_blank"
           )
         )
