22e258a891381d7a11ec94396ef483415e973e65
daattalitest <daattali+test@gmail.com>
2026-02-07 19:28:23 -0500

    Update app_ui.R to 25

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,7 +14,7 @@ app_ui <- function() {
           tags$a(
             href = config$help_link,
             id = "help_link",
-            "Examples and Help GTA7",
+            "Examples and Help GTA25",
             target = "_blank"
           )
         )
